7e3b612fe6385f9de1c48ae330aa5dcf650a5acf
philcantcode <20229771+philcantcode@users.noreply.github.com>
2021-12-09 16:35:05 +0000

    Add files via upload

---
+++ LaunchNotes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Launching NotNotes"
+java --module-path "C:\Program Files\Java\javafx-sdk-13.0.1\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar "E:\Google Drive\NodeNotes.jar"
